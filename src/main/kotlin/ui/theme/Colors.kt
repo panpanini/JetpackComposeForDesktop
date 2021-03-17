023b0ead3866545676d6ac69f30170a76b699816
panini <panpanini@mercari.com>
2021-03-17 11:20:30 +0900

    Add Theme & Colors

---
+++ src/main/kotlin/ui/theme/Colors.kt
@@ -0,0 +1,7 @@
+package ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryBlue = Color(0xFF5E6DF2)
+val primaryVariant = Color(0xFF87A7FF)
+val secondary = Color(0xFF444444)
---
+++ src/main/kotlin/ui/theme/Theme.kt
@@ -0,0 +1,19 @@
+package ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+
+val colors = lightColors(
+    primary = primaryBlue,
+    primaryVariant = primaryVariant,
+    secondary = secondary
+)
+
+@Composable
+fun PresentationTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
+}
