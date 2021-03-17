b8c7b5c0a1489270cab50dc3d25ebc92efec9519
panini <panpanini@mercari.com>
2021-03-17 12:19:01 +0900

    Add ContentScreen slide

---
+++ src/main/kotlin/ContentScreen.kt
@@ -0,0 +1,25 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ContentScreen(title: String, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Slide(modifier) {
+        Column(modifier = Modifier.fillMaxSize().padding(top = 64.dp, start = 64.dp)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.h2,
+                color = MaterialTheme.colors.secondary,
+                modifier = Modifier.padding(bottom = 64.dp)
+            )
+            content()
+        }
+
+    }
+}
